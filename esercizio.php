c0592ae90acdc6edf88ff56e0fe2685581cae911
Matteo Andreoli <maceo.andreoli@gmail.com>
2019-09-08 13:49:33 +0200

    Esercizio completato

--- esercizio.php
+++ esercizio.php
@@ -4,37 +4,61 @@
     <meta charset="utf-8">
     <title>Teacher Array</title>
     <style>
-    	/*p {font-size: 30px;margin-top: 40px;}*/
+    	body {font-size: 30px;    font-family: arial, sans-serif;}
+        .rect {
+            display: inline-block;
+            padding: 15px 20px;
+            border: 1px solid black;
+            margin: 10px;
+        }
+        .teacher {background-color: darkgrey;}
+        .pm {background-color: lightgreen;}
+
     </style>
   </head>
   <body>
   	<div id="main-container">
-		<p>
-			<?php 
-                $db = [
-                        'teachers' => [
-                            [
-                                'name' => 'Michele',
-                                'lastname' => 'Papagni'
-                            ],
-                            [
-                                'name' => 'Fabio',
-                                'lastname' => 'Forghieri'
-                            ]
-                        ],
-                        'pm' => [
-                            [
-                                'name' => 'Roberto',
-                                'lastname' => 'Marazzini'
-                            ],
-                            [
-                                'name' => 'Federico',
-                                'lastname' => 'Pellegrini'
-                            ]
-                        ]
-                    ];
-	  		?>
-  		</p>
+		<?php 
+            $db = [
+                'teachers' => [
+                    [
+                        'name' => 'Michele',
+                        'lastname' => 'Papagni'
+                    ],
+                    [
+                        'name' => 'Fabio',
+                        'lastname' => 'Forghieri'
+                    ]
+                ],
+                'pm' => [
+                    [
+                        'name' => 'Roberto',
+                        'lastname' => 'Marazzini'
+                    ],
+                    [
+                        'name' => 'Federico',
+                        'lastname' => 'Pellegrini'
+                    ]
+                ]
+            ];
+
+
+            // stampo insegnanti
+            foreach ($db['teachers'] as $teacher) {
+                $name = $teacher['name'];
+                $lastname = $teacher['lastname'];
+                echo '<p class="rect teacher">' . $name . ' ' . $lastname . '</p>';
+            }
+
+            echo "<br>";
+
+            // stampo pm
+            foreach ($db['pm'] as $pm) {
+                $name = $pm['name'];
+                $lastname = $pm['lastname'];
+                echo '<p class="rect pm">' . $name . ' ' . $lastname . '</p>';
+            }
+  		?>
   	</div>
   </body>
 </html>
